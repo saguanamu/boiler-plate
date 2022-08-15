5452065cfc008938b387f4d8d56117c40f225521
KyeongNa Park <61577469+pkn0814@users.noreply.github.com>
2022-08-16 05:26:18 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require("./models/User");
+
+// aplication/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://park:0814@cluster0.mmd8fvm.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(()=> console.log('MongoDB Connected'))
     .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send("Hello world!"))
+
+
+app.get('/', (req, res) => res.send("d world!"))
+
+app.post("/register", (req, res)=> {
+    // 회원 가입 시 필요한 정보를 client에서 가져오면 DB에 넣음
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+
+
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- models/User.js
+++ models/User.js
@@ -8,7 +8,7 @@ const userSchema = mongoose.Schema({
     email: {
         type: String,
         trim: true,
-        unique: 5
+        unique: 1
     },
     lastname: {
         type: String,
